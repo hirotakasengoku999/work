e559435eab90fab455ae40557915ca2d01f600e9
hirotakasengoku999 <hirotakasengoku@gmail.com>
2026-03-01 06:36:20 +0900

    rm file

--- aireceiptapp/test_pandas.py
+++
@@ -1,6 +0,0 @@
-import update_chart
-
-max_seikyunengtsu = update_chart.get_max_seikyunengetsu()
-
-table_name = 'aireceiptapp_backup_kenchikekka'
-update_chart.write_add_calc(table_name, max_seikyunengtsu)
